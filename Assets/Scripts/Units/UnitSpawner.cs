adfe1393d3315599fb20c44d39587e81df9d4706
ianlabrie <ianlabrie@gmail.com>
2022-08-03 03:59:40 -0400

    cleaning up some comments

--- Assets/Scripts/Units/UnitSpawner.cs
+++ Assets/Scripts/Units/UnitSpawner.cs
@@ -15,14 +15,14 @@ namespace HoH_StateManagerTest.Units
         List<Minion> MinionHolder;
         void Awake()
         {
-            instance = this; // should be refactored to not need this, just an easy hack for now
+            instance = this; // should be refactored, just an easy hack for now
             MinionTypes = MinionLoaderXML.LoadData();
             MinionHolder = new List<Minion>();
         }
 
         public void SpawnMinions(int minionCount)
         {
-            Debug.Log($"Spawning {minionCount} random minions"); // should use a Util Logger
+            Debug.Log($"Spawning {minionCount} random minions"); // logging and random numbers should come from a Util class
             for (int i = 0; i < minionCount; i++)
             {
                 Minion minion = Instantiate(MinionPrefab);
